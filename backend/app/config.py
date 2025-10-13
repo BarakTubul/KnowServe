2dcdabb8f341944f9b74cafb96a30f54020f1cbe
97252 <barak.tubul@gmail.com>
2025-10-13 14:53:35 +0300

    Added .env and integrated it to config

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,10 +1,16 @@
 # app/config.py
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "KnowServe"
-    ENVIRONMENT: str = "development"
+    PROJECT_NAME: str
+    ENVIRONMENT: str
 
+    model_config = SettingsConfigDict(
+        env_file=".env",          # Load variables from .env automatically
+        env_file_encoding="utf-8"
+    )
+
+# Instantiate settings
 settings = Settings()
+
 print(f"⚙️ Loaded settings for {settings.PROJECT_NAME} [{settings.ENVIRONMENT}]")
-print(f"⚙️ Loaded settings!")
