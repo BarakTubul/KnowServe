acadf7bc6b9a27f5042e41c1c82207d796466889
97252 <barak.tubul@gmail.com>
2025-11-19 15:32:38 +0200

    unit of work is completed

---
+++ backend/app/core/unit_of_work.py
@@ -0,0 +1,28 @@
+# app/core/unit_of_work.py
+from contextlib import AbstractContextManager
+from app.core.database import SessionLocal
+from app.repositories.document_repository import DocumentRepository
+from app.repositories.department_repository import DepartmentRepository
+
+class UnitOfWork(AbstractContextManager):
+    def __init__(self):
+        self.session = None
+        self.documents = None
+        self.departments = None
+
+    def __enter__(self):
+        self.session = SessionLocal()
+
+        # Attach repositories
+        self.documents = DocumentRepository(self.session)
+        self.departments = DepartmentRepository(self.session)
+
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type is None:
+            self.session.commit()
+        else:
+            self.session.rollback()
+
+        self.session.close()
